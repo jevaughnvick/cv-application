4637a15f7f22a54e23f7fdc3d40dd5f6ed3763cb
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-12-16 16:58:54 -0500

    write cv html

---
+++ src/components/CV.jsx
@@ -0,0 +1,35 @@
+export default function CV(){
+
+    return (
+        <div className="cv">
+            <section>
+                <h1>
+                    Jevaughn Vickers
+                </h1>
+                <h3>Software Developer</h3>
+                <p>1 (555) 825-6374</p>
+                <p>jevaughn@meteor.com</p>
+            </section>
+            <section>
+                <h2>Education</h2>
+                <h4>Institution</h4>
+                <p>Florida International University</p>
+                <h4>Programme</h4>
+                <p>Bachelor of Science in Computer Engineering</p>
+                <h4>Programme Duration</h4>
+                <p>September 6, 2025 - June 19, 2029</p>
+            </section>
+            <section>
+                <h2>Professional Experience</h2>
+                <h4>Company</h4>
+                <p>Main Origin</p>
+                <h4>Profession</h4>
+                <p>Chief Developer</p>
+                <h4>Job Description</h4>
+                <p>Head of software development department</p>
+                <h4>Employment Duration</h4>
+                <p>January 24, 2025 - present</p>
+            </section>
+        </div>
+    )
+}
